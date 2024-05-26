293ab5aa50ea1945748c34aed7f9c0b4262ffe1d
Hugo Henrique Oliveira Franklin Leite <henrique.hugo.9@hotmail.com>
2024-05-26 01:54:49 -0300

    att.route-api

--- CheapGames/app/Http/Controllers/Api/CategoryController.php
+++ CheapGames/app/Http/Controllers/Api/CategoryController.php
@@ -1,5 +1,6 @@
 <?php
 
+// Importando as classes necessárias
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
@@ -8,70 +9,72 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-   
+    // Função para listar todas as categorias
     public function index()
     {
-        $category = Category::all();
-        return response()->json(['category' => $category]);
+        $category = Category::all(); // Recupera todas as categorias
+        return response()->json(['category' => $category]); // Retorna as categorias como JSON
     }
     
+    // Função para criar uma nova categoria
     public function store(Request $request)
     {
-        $name = strtoupper($request->Name);
-        $existingCategory = Category::where('Name', $name)->first();
+        $name = strtoupper($request->Name); // Converte o nome da categoria para maiúsculas
+        $existingCategory = Category::where('Name', $name)->first(); // Verifica se a categoria já existe
     
-        if ($existingCategory) {
+        if ($existingCategory) { // Se a categoria já existir, retorna um erro
             return response()->json(['error' => 'Categoria já existe'], 400);
         }
     
-        $category = new Category;
-        $category->Name = $name;
-        $category->save();
+        $category = new Category; // Cria uma nova categoria
+        $category->Name = $name; // Define o nome da categoria
+        $category->save(); // Salva a categoria no banco de dados
     
-        return response()->json($category, 201);
+        return response()->json($category, 201); // Retorna a categoria criada como JSON
     }
     
+    // Função para exibir uma categoria específica
     public function show(string $id)
     {
-        $category = Category::find($id);
-        if ($category) {
+        $category = Category::find($id); // Recupera a categoria pelo ID
+        if ($category) { // Se a categoria existir, retorna a categoria como JSON
             return response()->json($category);
-        } else {
+        } else { // Se a categoria não existir, retorna um erro
             return response()->json(['error' => 'Categoria não encontrada'], 404);
         }
     }
     
+    // Função para atualizar uma categoria existente
     public function update(Request $request, string $id)
     {
-    $name = strtoupper($request->Name);
-    $existingCategory = Category::where('Name', $name)->first();
+        $name = strtoupper($request->Name); // Converte o nome da categoria para maiúsculas
+        $existingCategory = Category::where('Name', $name)->first(); // Verifica se a categoria já existe
 
-    if ($existingCategory && $existingCategory->CategoriaID != $id) {
-        return response()->json(['error' => 'Categoria já existe'], 400);
-    }
+        if ($existingCategory && $existingCategory->CategoriaID != $id) { // Se a categoria já existir e não for a categoria que está sendo atualizada, retorna um erro
+            return response()->json(['error' => 'Categoria já existe'], 400);
+        }
 
-    $category = Category::find($id);
-    if ($category) {
-        $category->Name = $name;
-        $category->save();
+        $category = Category::find($id); // Recupera a categoria pelo ID
+        if ($category) { // Se a categoria existir
+            $category->Name = $name; // Atualiza o nome da categoria
+            $category->save(); // Salva a categoria no banco de dados
 
-        return response()->json($category);
-    } else {
-        return response()->json(['error' => 'Categoria não encontrada'], 404);
-    }
-    }
-
-    
-    public function destroy(string $id)
-    {
-        $category = Category::find($id);
-        if ($category) {
-            $category->delete();
-    
-            return response()->json(['success' => 'Categoria deletada com sucesso']);
-        } else {
+            return response()->json($category); // Retorna a categoria atualizada como JSON
+        } else { // Se a categoria não existir, retorna um erro
             return response()->json(['error' => 'Categoria não encontrada'], 404);
         }
     }
-    
+
+    // Função para deletar uma categoria existente
+    public function destroy(string $id)
+    {
+        $category = Category::find($id); // Recupera a categoria pelo ID
+        if ($category) { // Se a categoria existir
+            $category->delete(); // Deleta a categoria
+
+            return response()->json(['success' => 'Categoria deletada com sucesso']); // Retorna uma mensagem de sucesso
+        } else { // Se a categoria não existir, retorna um erro
+            return response()->json(['error' => 'Categoria não encontrada'], 404);
+        }
+    }
 }
--- CheapGames/app/Http/Controllers/Api/PostController.php
+++ CheapGames/app/Http/Controllers/Api/PostController.php
@@ -1,5 +1,6 @@
 <?php
 
+// Importando as classes necessárias
 namespace App\Http\Controllers\Api;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
@@ -9,16 +10,15 @@ use Illuminate\Http\Request;
 use App\Models\Posts;
 use App\Models\Images;
 
-
 class PostController extends Controller
 {
-    
+    // Função para listar todos os posts
     public function index()
     {
-
+        // Recupera todos os posts com suas respectivas relações
         $posts = Posts::with('category', 'platform', 'images', 'ratings.user')->get();
 
-        // Verificando se os posts foram recuperados corretamente
+        // Verifica se os posts foram recuperados corretamente
         if ($posts->isEmpty()) {
             return response()->json([
                 'status' => 404,
@@ -26,18 +26,18 @@ class PostController extends Controller
             ]);
         }
 
-        // Adicionando likes, dislikes e nomes dos usuários à resposta
+        // Adiciona likes, dislikes e nomes dos usuários à resposta
         foreach ($posts as $post) {
             $ratingsSum = $post->ratingsSum();
             $post->likes = $ratingsSum['likes'];
             $post->dislikes = $ratingsSum['dislikes'];
 
-            // Adicionando os nomes dos usuários que deram like e dislike
+            // Adiciona os nomes dos usuários que deram like e dislike
             $post->userLikes = $post->ratings->where('Liked', 1)->pluck('user.Username');
             $post->userDislikes = $post->ratings->where('Liked', 0)->pluck('user.Username');
         }
 
-        // Removendo o atributo 'ratings' do post
+        // Remove o atributo 'ratings' do post
         $posts->makeHidden('ratings');
 
         return response()->json([
@@ -46,14 +46,10 @@ class PostController extends Controller
         ]);
     }
     
-    
-    
-   
+    // Função para criar um novo post
     public function store(Request $request)
     {
-
-        //dd($request);
-        // Validação dos dados recebidos
+        // Valida os dados recebidos
         $request->validate([
             'UserID' => 'required|integer',
             'Title' => 'required|string|max:255',
@@ -65,38 +61,38 @@ class PostController extends Controller
             'NewPrice' => 'required|numeric',
             'OldPrice' => 'required|numeric',
             'Link' => 'nullable|url',
-            //'ImageURL' => 'required|string|max:255',
-            // Adicione outras regras de validação conforme necessário
         ]);
 
-    
-        
+        // Verifica se foi enviada uma imagem
+        if ($request->input('ImageURL') == null){
+            $imgurURL = "https://imgur.com/a/5DKF8aR";
+        } else {
             // Faz o upload da imagem para o Imgur
             $client = new Client();
-                try {
-                    $response = $client->POST('https://api.imgur.com/3/image', [
-                        'headers' => [
-                            'Authorization' => 'Client-ID 19703dbb1c14e1f',
+            try {
+                $response = $client->POST('https://api.imgur.com/3/image', [
+                    'headers' => [
+                        'Authorization' => 'Client-ID 19703dbb1c14e1f',
+                    ],
+                    'multipart' => [
+                        [
+                            'name' => 'image',
+                            'contents' => fopen($request->file('ImageURL')->getPathname(), 'r'),
                         ],
-                        'multipart' => [
-                            [
-                                'name' => 'image',
-                                'contents' => fopen($request->file('ImageURL')->getPathname(), 'r'),
-                            ],
-                        ],
-                        'http_errors' => true,
-                    ]);
+                    ],
+                    'http_errors' => true,
+                ]);
 
-                    $responseBody = $response->getBody()->getContents();
-                    $imgurData = json_decode($responseBody, true);
-                    $imgurURL = $imgurData['data']['link'];
-                } catch (ClientException $e) {
-                    $errorResponse = $e->getResponse();
-                    $errorBody = $errorResponse->getBody()->getContents();
-                
-                }
+                $responseBody = $response->getBody()->getContents();
+                $imgurData = json_decode($responseBody, true);
+                $imgurURL = $imgurData['data']['link'];
+            } catch (ClientException $e) {
+                $errorResponse = $e->getResponse();
+                $errorBody = $errorResponse->getBody()->getContents();
+            }
+        }
 
-        // Crie um novo post
+        // Cria um novo post
         $post = Posts::create([
             'UserID' => $request->input('UserID'),
             'Title' => $request->input('Title'),
@@ -108,34 +104,27 @@ class PostController extends Controller
             'NewPrice' => $request->input('NewPrice'),
             'OldPrice' => $request->input('OldPrice'),
             'Link' => $request->input('Link')
-            // Preencha outros campos conforme necessário
         ]);
+
+        // Se a imagem foi enviada, cria uma nova imagem associada ao post
         if($imgurURL){
             $post->images()->create([
                 'ImageURL' => $imgurURL,
                 'Active' => true,
                 'Date' => now()
             ]);
-        }else{
-            $imgurURL =('default_image.jpg');
-            $post->images()->create([
-                'ImageURL' => $imagePath,
-                'Active' => true,
-                'Date' => now()
-            ]);
         }
-    
-        
 
         return response()->json(['message' => 'Post criado com sucesso!', 'post' => $post]);
     }
 
-   
-
+    // Função para exibir um post específico
     public function show(string $id)
     {
+        // Recupera o post pelo ID com suas respectivas relações
         $post = Posts::with('category', 'platform', 'images', 'ratings.user')->find($id);
     
+        // Verifica se o post foi recuperado corretamente
         if (!$post) {
             return response()->json([
                 'status' => 404,
@@ -143,130 +132,128 @@ class PostController extends Controller
             ]);
         }
     
+        // Adiciona likes, dislikes e nomes dos usuários à resposta
         $ratingsSum = $post->ratingsSum();
         $post->likes = $ratingsSum['likes'];
         $post->dislikes = $ratingsSum['dislikes'];
     
-        // Adicionando os nomes dos usuários que deram like e dislike
+        // Adiciona os nomes dos usuários que deram like e dislike
         $post->userLikes = $post->ratings->where('Liked', 1)->pluck('user.Username');
         $post->userDislikes = $post->ratings->where('Liked', 0)->pluck('user.Username');
     
-        // Removendo o atributo 'ratings' do post
+        // Remove o atributo 'ratings' do post
         unset($post->ratings);
     
         return response()->json([
             'status' => 200,
             'post' => $post,
         ]);
-    }
-    
-
-
-    
-    
+    } 
     
 
   
+    // Função para atualizar um post existente
     public function update(Request $request, string $id)
     {   
-
-    $request->validate([
-        'UserID' => 'required|integer',
-        'Title' => 'required|string|max:255',
-        'Description' => 'required|string',
-        'Active' => 'required|boolean',
-        'Date' => 'required|date',
-        'CategoryID' => 'required|integer',
-        'PlatformID' => 'required|integer',
-        'NewPrice' => 'required|numeric',
-        'OldPrice' => 'required|numeric',
-        'Link' => 'nullable|url',
-        //'ImageURL' => 'required|string|max:255',
-        // Adicione outras regras de validação conforme necessário
-    ]);
-
-    $client = new Client();
-    try {
-        $response = $client->POST('https://api.imgur.com/3/image', [
-            'headers' => [
-                'Authorization' => 'Client-ID 19703dbb1c14e1f',
-            ],
-            'multipart' => [
-                [
-                    'name' => 'image',
-                    'contents' => fopen($request->file('ImageURL')->getPathname(), 'r'),
-                ],
-            ],
-            'http_errors' => true,
+        // Valida os dados recebidos
+        $request->validate([
+            'UserID' => 'required|integer',
+            'Title' => 'required|string|max:255',
+            'Description' => 'required|string',
+            'Active' => 'required|boolean',
+            'Date' => 'required|date',
+            'CategoryID' => 'required|integer',
+            'PlatformID' => 'required|integer',
+            'NewPrice' => 'required|numeric',
+            'OldPrice' => 'required|numeric',
+            'Link' => 'nullable|url',
         ]);
 
-        $responseBody = $response->getBody()->getContents();
-        $imgurData = json_decode($responseBody, true);
-        $imgurURL = $imgurData['data']['link'];
-    } catch (ClientException $e) {
-        $errorResponse = $e->getResponse();
-        $errorBody = $errorResponse->getBody()->getContents();
-      
-    }
+        // Verifica se foi enviada uma imagem
+        if ($request->input('ImageURL') == null){
+            $imgurURL = "https://imgur.com/a/5DKF8aR";
+        } else {
+            // Faz o upload da imagem para o Imgur
+            $client = new Client();
+            try {
+                $response = $client->POST('https://api.imgur.com/3/image', [
+                    'headers' => [
+                        'Authorization' => 'Client-ID 19703dbb1c14e1f',
+                    ],
+                    'multipart' => [
+                        [
+                            'name' => 'image',
+                            'contents' => fopen($request->file('ImageURL')->getPathname(), 'r'),
+                        ],
+                    ],
+                    'http_errors' => true,
+                ]);
 
-    $post = Posts::find($id);
-    if (!$post) {
-        return response()->json(['message' => 'Recurso não encontrado'], 404);
-    }
-    
-
-    if ($post) {
-        // Atualize os campos relevantes com base nos dados do $request
-        $post->update([
-            'Title' => $request->input('Title'),
-            'Description' => $request->input('Description'),
-            'Active' => true,
-            'Date' => $request->input('Date'),
-            'CategoryID' => $request->input('CategoryID'),
-            'PlatformID' => $request->input('PlatformID'),
-            'NewPrice' => $request->input('NewPrice'),
-            'OldPrice' => $request->input('OldPrice'),
-            'Link' => $request->input('Link')
-            // Preencha outros campos conforme necessário
-        ]);
-
-        if($imgurURL){
-            $post->images()->update([
-                'ImageURL' => $imgurURL
-            ]);
-        }else{
-            $imgurURL =('default_image.jpg');
-            $post->images()->update([
-                'ImageURL' => $imagePath
-            ]);
+                $responseBody = $response->getBody()->getContents();
+                $imgurData = json_decode($responseBody, true);
+                $imgurURL = $imgurData['data']['link'];
+            } catch (ClientException $e) {
+                $errorResponse = $e->getResponse();
+                $errorBody = $errorResponse->getBody()->getContents();
+            }
         }
-        
-        return response()->json($post);
-    } else {
-        return response()->json(['error' => 'Post não encontrado'], 404);
-    }
-    
-    }
 
-  
-    public function destroy(string $id)
-    {
+        // Recupera o post pelo ID
         $post = Posts::find($id);
         if (!$post) {
             return response()->json(['message' => 'Recurso não encontrado'], 404);
-            }
-            if ($post->Active == true) {
-                $post->update([
-                    'Active' => false,
-                    ]);
+        }
+        
+        // Se o post existir, atualiza os campos relevantes
+        if ($post) {
+            $post->update([
+                'Title' => $request->input('Title'),
+                'Description' => $request->input('Description'),
+                'Active' => true,
+                'Date' => $request->input('Date'),
+                'CategoryID' => $request->input('CategoryID'),
+                'PlatformID' => $request->input('PlatformID'),
+                'NewPrice' => $request->input('NewPrice'),
+                'OldPrice' => $request->input('OldPrice'),
+                'Link' => $request->input('Link')
+            ]);
 
-                    return response()->json(['message' => 'Post deletado com sucesso'], 200);
-            }else{
-                $post->update([
-                    'Active' => true,
-                    ]);
-
-                    return response()->json(['message' => 'Post Ativo com sucesso'], 200);
+            // Se a imagem foi enviada, atualiza a imagem associada ao post
+            if($imgurURL){
+                $post->images()->update([
+                    'ImageURL' => $imgurURL
+                ]);
             }
+            
+            return response()->json($post);
+        } else {
+            return response()->json(['error' => 'Post não encontrado'], 404);
+        }
+    }
+
+    // Função para deletar um post existente
+    public function destroy(string $id)
+    {
+        // Recupera o post pelo ID
+        $post = Posts::find($id);
+        if (!$post) {
+            return response()->json(['message' => 'Recurso não encontrado'], 404);
+        }
+
+        // Se o post estiver ativo, desativa o post
+        if ($post->Active == true) {
+            $post->update([
+                'Active' => false,
+            ]);
+
+            return response()->json(['message' => 'Post deletado com sucesso'], 200);
+        } else {
+            // Se o post estiver inativo, ativa o post
+            $post->update([
+                'Active' => true,
+            ]);
+
+            return response()->json(['message' => 'Post Ativo com sucesso'], 200);
+        }
     }
 }
